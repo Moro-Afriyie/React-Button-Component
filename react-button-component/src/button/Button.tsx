c33c5557f3c43d7357af377e685bc716645e1f15
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-19 13:15:14 -0100

    create button component

---
+++ react-button-component/src/button/Button.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface IButtonProps {}
+
+const Button: React.FunctionComponent<IButtonProps> = (props) => {
+  return <button>Button</button>;
+};
+
+export default Button;
