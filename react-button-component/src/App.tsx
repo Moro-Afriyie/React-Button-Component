69bc426acf5715bd450cc6916aa5ccaf2b53708d
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-19 15:13:17 -0100

    done with the buttons

--- react-button-component/src/App.tsx
+++ react-button-component/src/App.tsx
@@ -26,13 +26,73 @@ function App() {
           <p className="title">&#60;Button disableShadow /&#62;</p>
           <Button disableShadow />
         </div>
-        <div className="button__variant">
-          <p className="title">&#60;Button disabled/&#62;</p>
+        <div className="button__disabled">
+          <p className="title">&#60;Button disabled /&#62;</p>
           <Button disabled />
           <p className="title">
-            &#60;Button variant=<q>text</q> disabled/&#62;
+            &#60;Button variant=<q>text</q> disabled /&#62;
           </p>
           <Button variant="text" disabled />
+        </div>
+        <div className="button__startIcon">
+          <div className="right">
+            <p className="title">
+              &#60;Button startIcon=<q>local_grocery_store</q> /&#62;
+            </p>
+            <Button startIcon="local_grocery_store" />
+          </div>
+          <div className="left">
+            <p className="title">
+              &#60;Button endIcon=<q>local_grocery_store</q> /&#62;
+            </p>
+            <Button endIcon="local_grocery_store" />
+          </div>
+        </div>
+        <div className="button__size">
+          <div className="small">
+            <p className="title">
+              &#60;Button size=<q>sm</q> /&#62;
+            </p>
+            <Button size="sm" />
+          </div>
+          <div className="medium">
+            <p className="title">
+              &#60;Button size=<q>md</q> /&#62;
+            </p>
+            <Button size="md" />
+          </div>
+          <div className="large">
+            <p className="title">
+              &#60;Button size=<q>lg</q> /&#62;
+            </p>
+            <Button size="lg" />
+          </div>
+        </div>
+        <div className="button__colors">
+          <div className="color__default">
+            <p className="title">
+              &#60;Button color=<q>default</q> /&#62;
+            </p>
+            <Button color="default" />
+          </div>
+          <div className="color__primary">
+            <p className="title">
+              &#60;Button color=<q>primary</q> /&#62;
+            </p>
+            <Button color="primary" />
+          </div>
+          <div className="color__secondary">
+            <p className="title">
+              &#60;Button color=<q>secondary</q> /&#62;
+            </p>
+            <Button color="secondary" />
+          </div>
+          <div className="color__danger">
+            <p className="title">
+              &#60;Button color=<q>danger</q> /&#62;
+            </p>
+            <Button color="danger" />
+          </div>
         </div>
       </div>
     </main>
--- react-button-component/src/button/Button.tsx
+++ react-button-component/src/button/Button.tsx
@@ -5,12 +5,20 @@ interface IButtonProps {
   variant?: string;
   disableShadow?: true;
   disabled?: true;
+  startIcon?: string;
+  endIcon?: string;
+  size?: string;
+  color?: string;
 }
 
 const Button: React.FunctionComponent<IButtonProps> = ({
   variant,
   disableShadow,
   disabled,
+  startIcon,
+  endIcon,
+  size,
+  color,
 }: IButtonProps) => {
   if (variant === "outline") {
     return <button className="outline">Default</button>;
@@ -26,6 +34,30 @@ const Button: React.FunctionComponent<IButtonProps> = ({
         Disabled
       </button>
     );
+  } else if (startIcon) {
+    return (
+      <button className="startIcon">
+        <span className="material-icons">{startIcon}</span> Default
+      </button>
+    );
+  } else if (endIcon) {
+    return (
+      <button className="startIcon">
+        Default <span className="material-icons">{endIcon}</span>
+      </button>
+    );
+  } else if (size === "sm") {
+    return <button className="sm">Default</button>;
+  } else if (size === "md") {
+    return <button className="md">Default</button>;
+  } else if (size === "lg") {
+    return <button className="lg">Default</button>;
+  } else if (color === "primary") {
+    return <button className="primary">Default</button>;
+  } else if (color === "secondary") {
+    return <button className="secondary">Default</button>;
+  } else if (color === "danger") {
+    return <button className="danger">Default</button>;
   }
   return (
     <button className={disabled ? "default disabled" : "default"}>
